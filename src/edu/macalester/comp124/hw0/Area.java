2593051e1028d1406529cb41eee8b0b9bd703ae4
hkuramochi <haruhiko.k@gmail.com>
2018-09-04 14:46:42 -0500

    Fixed formulas for area of a square and a circle

--- src/edu/macalester/comp124/hw0/Area.java
+++ src/edu/macalester/comp124/hw0/Area.java
@@ -1,5 +1,7 @@
 package edu.macalester.comp124.hw0;
 
+import static java.lang.Math.PI;
+
 /**
  * @author Shilad Sen
  */
@@ -11,7 +13,7 @@ public class Area {
      */
     public static double getSquareArea(double sideLength) {
         // TODO: replace me with the correct formula
-        return sideLength * 1.0 * 42 +13;
+        return sideLength * sideLength;
     }
 
     /**
@@ -21,7 +23,7 @@ public class Area {
      */
     public static double getCircleArea(double radius) {
         // TODO: replace me with the correct formula
-        return radius * 1.0;
+        return radius * radius * PI;
     }
 
 }
